6badeb4273694b0dcdd21d639707b8326a275341
dawbit <dawbitner@gmail.com>
2019-03-22 14:15:03 +0100

    separate code and add .gitignore

---
+++ core/neural_network1.py
@@ -0,0 +1,84 @@
+import numpy as np
+import tensorflow as tf
+import tflearn
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+
+TRAIN_DIR = 'train'
+TEST_DIR = 'test'
+IMG_SIZE = 125
+LR = 1e-3
+MODEL_NAME = 'meowfinder-{}-{}'.format(LR, 'basic')
+
+train_data = np.load('train_data.npy')
+test_data = np.load('test_data.npy')
+
+train = train_data[:-5000]
+test = train_data[-5000:]
+
+def network1():
+    X_train = np.array([i[0] for i in train]).reshape(-1, IMG_SIZE, IMG_SIZE, 1)#oryginalna wersja#
+    #X_train = np.array([i[0] for i in train]).reshape(-1, IMG_SIZE)#
+    y_train = [i[1] for i in train]
+
+    X_test = np.array([i[0] for i in test]).reshape(-1, IMG_SIZE, IMG_SIZE, 1)#oryginalna wersja#
+    #X_test = np.array([i[0] for i in test]).reshape(-1, IMG_SIZE)#
+    y_test = [i[1] for i in test]
+
+    tf.reset_default_graph()
+
+    convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
+
+    convnet = conv_2d(convnet, 32, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 64, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = fully_connected(convnet, 1024, activation='relu')
+    convnet = dropout(convnet, 0.8)
+
+    convnet = fully_connected(convnet, 2, activation='softmax')
+    convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+
+    network1.model = tflearn.DNN(convnet, tensorboard_dir='log', tensorboard_verbose=0)
+
+    network1.model.fit({'input': X_train}, {'targets': y_train}, n_epoch=10,
+              validation_set=({'input': X_test}, {'targets': y_test}),
+              snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+
+    tf.reset_default_graph()
+
+    convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
+
+    convnet = conv_2d(convnet, 32, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 64, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 128, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 64, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 32, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = fully_connected(convnet, 1024, activation='relu')
+    convnet = dropout(convnet, 0.8)
+
+    convnet = fully_connected(convnet, 2, activation='softmax')
+    convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+
+    network1.model = tflearn.DNN(convnet, tensorboard_dir='log', tensorboard_verbose=0)
+
+    network1.model.fit({'input': X_train}, {'targets': y_train}, n_epoch=10,
+              validation_set=({'input': X_test}, {'targets': y_test}),
+              snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+    #------ tej linijki na dole nie miales w pliku, ale byla w notatniku z jupitera------#
+    network1.model.save(MODEL_NAME)
+
+    #return network1.model#
